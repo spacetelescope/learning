198095a7cc07f2e54caf4955a8a802d528ba0e45
Justin Quick <jquick@stsci.edu>
2021-12-24 03:06:16 -0500

    added logging, index form to try search

--- chatbot/server.py
+++ chatbot/server.py
@@ -1,7 +1,7 @@
-from json import load
 from os import getenv
 from json import loads
-from flask import Flask, jsonify, request, abort
+import logging
+from flask import Flask, jsonify, request
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk
@@ -10,20 +10,15 @@ from elasticsearch_dsl import Search
 es = Elasticsearch(getenv('ES_HOSTS', 'localhost').split(','))
 ix = 'chatbot'
 app = Flask(__name__)
-
-
-def helper():
-    for doc in load(open('helps.json')).values():
-        es.index(index=ix, id=doc['title'], body=doc)
-    es.indices.refresh(index=ix)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def get_results(query, num):
     search = Search(using=es, index=ix).query("multi_match", query=query, fields=['title', 'content'])
     total = []
     for hit in search.execute():
-        print(f"{hit.meta.score}={hit.title}: {hit.content}"[:140])
-        total.append({'url': hit.url, 'title': hit.title, 'content': hit.content})
+        logging.debug(f"{hit.meta.score}={hit.title}: {hit.content}"[:140])
+        total.append({'url': hit.url, 'title': hit.title, 'content': ' '.join(hit.content.splitlines())})
         if num and len(total) == num:
             return total
     return total
@@ -35,14 +30,16 @@ def search():
     Searches the ES index for articles matching the `q` query parameter text
     Limits the results to `n` articles
     """
+    logging.debug(f'SEARCH {request.form}')
     query = request.form.get('q', '')
     num = int(request.form.get('n', '5'))
-    print(f'Query: {query} Limit: {num}')
+    logging.debug(f'Query: {query} Limit: {num}')
     return jsonify({'response': get_results(query, num)})
 
 
 @app.route('/index', methods=['POST'])
 def index():
+    logging.debug(f'INDEX {request.data}')
     try:
         data = loads(request.data)
     except ValueError:
@@ -61,3 +58,13 @@ def index():
         streaming_bulk(es, actions)
     es.indices.refresh(index=ix)
     return 'OK', 201
+
+
+@app.route('/', methods=['GET'])
+def form():
+    return '''
+    <form method="post" action="/search">
+      <label for="q">Query:</label><br>
+      <input type="text" id="q" name="q"><br>
+    </form>
+    ''', 200
